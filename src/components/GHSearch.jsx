8cb2691c45f14e37471b90a5268315a5af56b567
FacundoOs <osores.facundo@gmail.com>
2020-10-21 23:07:49 +0200

    Refactor. Fix the get request

--- src/components/GHSearch.jsx
+++ src/components/GHSearch.jsx
@@ -1,29 +1,42 @@
 import React, { Component } from "react";
 import { Button, Input } from "semantic-ui-react";
-import { getSearchUser} from "../modules/getSearchUser"
+// import { getSearchUser} from "../modules/getSearchUser"
+import axios from "axios";
 
 class GHSearch extends Component {
   state = {
-    items: {},
+    items: [],
   };
 
-  searchUser = async (e) => {
-    let result = await getSearchUser();
+  searchUser = async () => {
+    let response = await axios.get("https://api.github.com/search/users?q=tom");
+    debugger
     this.setState({
-      items: result,
+      items: response.data.items
     });
   };
 
   render() {
-    let items = this.state.items.login;
-
+    debugger
+    let dataShow = this.state.items.map((show) => {
+      return (
+        <div class="display-show" data-cy="display-show">
+          {show.login}
+        </div>
+      );
+    });
     return (
       <>
-        <Input data-cy="input" type="text" name="search" placeholder="Input GH username" />
+        <Input
+          data-cy="input"
+          type="text"
+          name="search"
+          placeholder="Input GH username"
+        />
         <Button onClick={this.searchUser} data-cy="search" name="search">
           Search
         </Button>
-    <p data-cy="user-data">{items}</p>
+        <p data-cy="user-data">{dataShow}</p>
       </>
     );
   }
